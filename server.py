8e80f4b50cd65c6a52edf971283c86c6ee45e711
Axel Colson <axel_colson@hotmail.com>
2025-04-17 20:22:00 +0200

    Add CORS middleware

--- server.py
+++ server.py
@@ -1,8 +1,18 @@
 from fastapi import FastAPI, UploadFile, File
 from fastapi.responses import StreamingResponse
+from fastapi.middleware.cors import CORSMiddleware
 from rembg import remove
 
 app = FastAPI()
+
+# Allow CORS
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # or ["http://localhost:5173"] if you want to restrict it
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.post("/remove-bg")
 async def remove_background(image: UploadFile = File(...)):
